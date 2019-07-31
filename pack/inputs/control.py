ffc55a595902dcfe8e99951c482895d2ddf1e388
Merlingot <49075262+Merlingot@users.noreply.github.com>
2019-07-31 23:44:55 +0200

    Update control.py

--- pack/inputs/control.py
+++ pack/inputs/control.py
@@ -1,27 +1,28 @@
-#File containing the infos to run the kmc simulation
-#TODO : saving frequency
+# Control File
+# Contains the infos to run the kmc simulation
+# TODO : allow setting of saving frequency
 
-# number of steps
+# number of kmc steps
 nsteps = 100
-# size of simulation
+# size of simulation (number of repetitions in x and y directions)
 nx = 20
 ny = 20
-# temperature of substrate
+# temperature of substrate (Kelvins)
 temperature=300
-# deposition rate
+# deposition rate (angstrom/s)
 deposition_rate=50
 
-# absolute path to the package directory (<mypath>/pack/)
+# absolute path to the package directory (<simudir>/pack/)
 packdir = "/Users/mariannelado-roy/kmc-simulation/pack/"
-
-# path to the file where the results of the simulation will be written
-resultFile = packdir + "out/results.txt"
 
 # path to the directory where the position of the atoms will be written
 coordDir = packdir + "out/coordinates/"
 
 # path to the directory where to save the frames
 frameDir = packdir + "out/frames/"
+
+# path to the file where the results of the simulation will be written
+resultFile = packdir + "out/results.txt"
 
 # path to the file where the domain lenght will be written
 rangeFile = packdir + "out/range.txt"
