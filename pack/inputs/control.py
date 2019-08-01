5e3c272d7959b12de4cec417a4bc9b6240124b4a
Merlingot <merlingot1@gmail.com>
2019-08-01 10:25:49 +0200

    Update control file

--- pack/inputs/control.py
+++ pack/inputs/control.py
@@ -11,23 +11,25 @@ temperature=300
 # deposition rate
 deposition_rate=50
 
-# absolute path to the package directory (<mypath>/pack/)
-packdir = "/Users/mariannelado-roy/kmc-simulation/pack/"
-
-# path to the file where the results of the simulation will be written
-resultFile = packdir + "out/results.txt"
+# absolute path to the simulation directory (<simudir>)
+simudir = "/Users/mariannelado-roy/kmc-simulation/"
+# absolute path to the package directory (<simudir>/pack/)
+packdir = simudir + "pack/"
 
 # path to the directory where the position of the atoms will be written
-coordDir = packdir + "out/coordinates/"
+coordDir = simudir + "out/coordinates/"
+
+# path to the file where the results of the simulation will be written
+resultFile = simudir + "out/results.txt"
 
 # path to the directory where to save the frames
-frameDir = packdir + "out/frames/"
+frameDir = simudir + "out/frames/"
 
 # path to the file where the domain lenght will be written
-rangeFile = packdir + "out/range.txt"
+rangeFile = simudir + "out/range.txt"
 
 # path and name of the file where the unregistered configurations will be written.
-undefinedFile = packdir + "out/undefined.txt"
+undefinedFile = simudir + "out/undefined.txt"
 
-# path to the gif file
-gifFile = packdir + "out/mygif.gif"
+# path and name of the gif file
+gifFile = simudir + "out/mygif.gif"
