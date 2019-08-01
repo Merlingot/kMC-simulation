9a5f2cade9bac11a4cade7ac4562e42f5f1ef6cf
Merlingot <merlingot1@gmail.com>
2019-08-01 10:55:57 +0200

    update createGif

--- createGif.py
+++ createGif.py
@@ -1,10 +1,10 @@
-#------------------------------------------------------
+#######################################################
 # Executable file : creatGif.py
 # Description: makes a gif from the simulation data
 # How to run:
-# $ cd <simudir>
-# $ python createGif.py
-#------------------------------------------------------
+#   $ cd <simudir>
+#   $ python createGif.py
+#######################################################
 
 import pack.inputs.control as ctrl
 from pack.tools.gifFuncs import createGif
