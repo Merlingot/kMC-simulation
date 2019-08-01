4d2afda065a396ac3f316d45b540223df748ec70
Merlingot <merlingot1@gmail.com>
2019-08-01 11:57:24 +0200

    animation working

--- pack/tools/gifFuncs.py
+++ pack/tools/gifFuncs.py
@@ -1,69 +1,70 @@
-#------------------------------------------------------
+#######################################################
 # Module: gifFuncs
 # Description: contains functions to make a gif out of
-# the frames saved during the simulation.
+# the data saved during the simulation.
 # Functions included:
-#    - animation
 #    - read_data
-#    - set_data
-#------------------------------------------------------
+#    - animation
+#       - put_data
+#       - init_anim
+#######################################################
 
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.animation import FuncAnimation
+import os
 
 def animation(coordDir, rangeFile, gifFile):
     """
     Function creating an animation and saving it.
     Args:
-        coordDir: absolute path to the directory containing the frames (frame = file containing the coordinates of atoms for each kmc step)
-        <packdir>/pack/out/coordinates/ or <control file>.coordDir
+        coordDir: absolute path to the directory containing the coordinates of atoms for each kmc step
+        <outdir>/coordinates/  or  <control file>.coordDir
 
         rangeFile: absolute path to the file containing the domain lenght
-        <packdir>/pack/out/range.txt or <control file>.rangeFile
+        <outdir>/range.txt  or  <control file>.rangeFile
 
         gifFile: absolute path, name and extension of the gif.
-        <packdir>/pack/out/<name>.gif or <control file>.gifFile
+        <outdir>/mygif.gif  or  <control file>.gifFile
     """
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    # read rangeFile to retrieve domain lenght and set xlims and ylim
-    lx, ly =
-    plt.xlim(lx*(-0.1), lx*1.1)
-    plt.ylim(ly*(-0.1), ly*1.1)
-
     fig, ax = plt.subplots()
     ax.set_aspect('equal')
-    scat = ax.scatter([],[], c=[], marker = '.', markersize=1, cmap=plt.cm.get_cmap('Blues', 4), vmin = -1, vmax = 4)
-    time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
-    time_text.set_text('')
 
-    # read dataFolder
-    # number of files in coordDir
-    nb_of_files =
+    lx, ly = np.genfromtxt(rangeFile, comments = '#', unpack=True)
+    ax.set_xlim([lx*(-0.1), lx*1.1])
+    ax.set_ylim([ly*(-0.1), ly*1.1])
+    text = ax.text(0.02, 1.0, '', transform=ax.transAxes)
 
-    anim=FuncAnimation( fig, set_data, frames=read_data(nb_of_files), interval = 300, blit = True, repeat = False)
+    line, = ax.plot([], [], 'o', markersize=1)
 
-    # save gif
-    save( '{}'.format(gifFile))
+    def init_anim():
+        line.set_data([], [])
+        text.set_text('')
+        return (line, text)
 
+    def put_data(frame):
+        line.set_data(frame[0],frame[1])
+        text.set_text('step={} time = {} s '.format(frame[3], frame[4]))
+        return (line, text)
 
-def read_data(n):
+    anim=FuncAnimation( fig, put_data, frames=read_data(coordDir), init_func=init_anim, interval = 500, blit = False, repeat = False)
+
+    plt.show()
+
+    return anim
+
+def read_data(coordDir):
     """update data"""
+    path, dirs, files = next(os.walk(coordDir))
+    n = len(files) # number of files in coordDir
     i=0
     while i < n:
-        # open file
-
-        # retrieve data
-        time =
-        x, y, occ =
-        yield x, y, occ, time, step
+        file = path+'step_{}.txt'.format(i+1)
+        x, y, occ = np.genfromtxt(file, comments='#', unpack=True)
+        with open( file, 'r') as f:
+            line1 = f.readline()
+            line2 = f.readline()
+        nstep = line1[7:]
+        time = line2[6:]
         i+=1
-
-def set_data(frame):
-    """set data to scatter plot"""
-    scat.set_array(frame[2])
-    scat.set_offsets(np.c_[frame[0],frame[1]])
-    text.set_text('time = {:.6f} \n step = {}'.format(frame[3], frame[4]))
-    return scat, text
+        yield x, y, occ, nstep, time
